e19923df31b9cd87f1ead77f8ac3e9569d681a9d
Ashley Chase <chaseashley91@gmail.com>
2022-07-05 23:46:31 -0700

    file assignment

---
+++ python/assignments/fileAssignment.py
@@ -0,0 +1,141 @@
+# this is a single line comment
+"""
+This is
+a multiline
+comment
+"""
+# my_new_favorite_language = 'python'
+
+"""'
+* Bonus: Errors
+
+- NameError: name <variable name> is not defined
+- TypeError: 'tuple' object does not support item assignment
+- KeyError: 'favorite_team'
+- IndexError: list index out of range
+- IndentationError: unexpected indent
+- AttributeError: 'tuple' object has no attribute 'pop'
+- AttributeError: 'tuple' object has no attribute 'append'
+- Tuples
+    - change value
+    - add value
+    - delete value
+"""
+
+num1 = 42 #variable declaration
+num2 = 2.3 #number
+boolean = True #single line, boolean
+string = 'Hello World'
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives'] #list
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
+fruit = ('blueberry', 'strawberry', 'banana')
+print(type(fruit))#- type check
+print(pizza_toppings[1]) #access value
+pizza_toppings.append('Mushrooms')  #add value
+print(person['name'])
+person['name'] = 'George' #change value
+person['eye_color'] = 'blue'
+print(fruit[2])
+
+if num1 > 45:
+    print("It's greater") #string
+else:
+    print("It's lower")
+
+"""
+- conditional
+    - if
+    - else if
+    - else"""
+
+if len(string) < 5: 
+    print("It's a short word!")
+elif len(string) > 15: #length check
+    print("It's a long word!")
+else:
+    print("Just right!")
+
+for x in range(5): #initialize
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(2,10,3):
+    print(x)
+x = 0
+
+while(x < 5):
+    print(x)
+    x += 1
+    """
+    - for loop
+    - start
+    - stop
+    - increment
+    - break
+    - continue
+    - sequence"""
+
+    """
+    - while loop
+    - start
+    - stop
+    - increment"""
+
+pizza_toppings.pop()
+pizza_toppings.pop(1)#delete value
+
+print(person)
+person.pop('eye_color')
+"""
+        - Tuples
+            - initialize
+            - access value
+            - change value
+            - add value
+            - delete value
+            """
+print(person)
+
+for topping in pizza_toppings:
+    if topping == 'Pepperoni':
+        continue
+    print('After 1st if statement')
+    if topping == 'Olives':
+        break
+"""- function
+    - parameter
+    - argument
+    - return"""
+
+def print_hello_ten_times():
+    for num in range(10):
+        print('Hello')
+
+print_hello_ten_times()
+
+def print_hello_x_times(x): #multiline
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_times(4)
+
+def print_hello_x_or_ten_times(x = 10):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
+
+
+"""
+Bonus section comment
+"""
+
+# print(num3)
+# num3 = 72
+# fruit[0] = 'cranberry'
+# print(person['favorite_team'])
+# print(pizza_toppings[7])
+#   print(boolean)
+# fruit.append('raspberry')
+# fruit.pop(1)
