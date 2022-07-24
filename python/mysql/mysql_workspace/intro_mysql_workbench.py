1cccb5cbd854f57190c76ef00cdeae1b7e3c628a
Ashley Chase <chaseashley91@gmail.com>
2022-07-24 13:50:52 -0700

    dojos and ninjas assignment text file

---
+++ python/mysql/mysql_workspace/intro_mysql_workbench.py
@@ -0,0 +1,11 @@
+
+#TODO WHAT IS MYSQL WORKBENCH?
+
+#TODO MySQL workbench is just a graphical user interface (GUI) for u to interact with mySQL. One of the mot popular relational
+#todo data bases in the world. - through QL command. It i not neceary to use a GUI. We may interact with our MySQL databases through  the terminal, but the 
+#todo GUI makes it much easier to ee our data and create new databaes.
+
+#todo why mySQL workbench? - we use when we are interacting wtith our databases because it will help us 
+#todo run some SQL queiries. It also has a great interface where we can map out our tables and establish relationships between them.
+
+#todo INSTALL MYSQL - follow link
