64ea499dc5b4224f550a2d62b6195136516a44a1
Ashley Chase <chaseashley91@gmail.com>
2022-07-31 18:05:41 -0700

    dojo survey assignment

---
+++ python/flask/fundamentals/dojo_survey/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, session, redirect, request
+import pprint 
+app = Flask(__name__)
+
+app.secret_key="keep it safe,keep it secret"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result',methods=['POST'])
+def result(): 
+    print("I got here 1")
+    print(request.form.to_dict())
+    print("I got here 2")
+    pprint.pprint(request.form)
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/complete')
+    # return render_template('form_result') #under def result
+
+@app.route('/complete')
+def page_2():
+    return render_template('complete.html')
+
+if __name__=="__main__":
+    app.run(debug=True, port =5001)
+#return render template
+#render template - new html page
+#included id in dojo dictionary? now
+
