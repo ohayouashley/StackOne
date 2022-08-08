92bcbc5039edb3a511b8eee78e98b26b678b3302
Ashley Chase <chaseashley91@gmail.com>
2022-08-07 23:32:23 -0700

    hw

---
+++ python/flask_mysql/dojos_and_ninjas/controller/user.py
@@ -0,0 +1,32 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+db = 'dojos_and_ninjas_schema'
+# model the class after the friend table from our database
+class User: #!capital letters identify classes - file typically matches the class name lowercase.
+    def __init__( self , data ): #! initialization/constructor method | data comes from data base
+        self.id = data['id'] #! should have 1 - 1 (match database)
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.age = data['age']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        # Now we use class methods to query our database
+    @classmethod #!gives us access to the cls in get_all method. | cls like self allows 
+    def get_all(cls): #! needs to have cls with @classmethod | direct reference to User
+        query = "SELECT * FROM users;" #!will always return a list if it works
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL(db).query_db(query)#!if you can't connect to your database your name may be wrong.
+        # Create an empty list to append our instances of friends
+        users = []
+# Iterate over the db results and create instances of friends with cls.
+
+        for row in results: #!represents a row of data
+            users.append( cls(row) ) #cls invoking instance of a class
+        return users
+
+    @classmethod #!needs a class method because you're calling the class User
+    def save(cls, data): #! want this to be a global method for all users, not individuals
+        query = "INSERT INTO users (first_name, last_name, age) VALUES (%(first_name)s, %(last_name)s, %(age)s)"
+
+        id = connectToMySQL(db).query_db(query, data) #!when you have a %()s you need data argument
+        return id
